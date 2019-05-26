0dc83d2b7774414d39b589629fcae352b23a97f5
Jake2019 <jakeu34@gmail.com>
2019-05-26 09:47:27 -0400

    Updates

---
+++ precache-manifest.58d775bb68e225ca71d2b9027dbef84e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "019ec502af1acdf9af3c356ac6c10b4f",
+    "url": "/JakeUnger/react-gh-pages.git/index.html"
+  },
+  {
+    "revision": "d8f6207fc8436ae4e1f2",
+    "url": "/JakeUnger/react-gh-pages.git/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/JakeUnger/react-gh-pages.git/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d8f6207fc8436ae4e1f2",
+    "url": "/JakeUnger/react-gh-pages.git/static/js/main.7f5ee66f.chunk.js"
+  },
+  {
+    "revision": "9444afbe6911afad1307",
+    "url": "/JakeUnger/react-gh-pages.git/static/js/runtime~main.f3b6db54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/JakeUnger/react-gh-pages.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
